e50bc6e9e7cb12b1ed9effbc6085f54c283a6b34
PARTH KANJIBHAI PARMAR <95738647+ParthParmar2112@users.noreply.github.com>
2023-04-25 23:22:12 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = 'parth';
+$database = 'asms';
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 if(!$conn){
